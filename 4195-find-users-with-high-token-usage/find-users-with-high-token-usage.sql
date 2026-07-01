141ca0a15ccef39ed8b56f4c98681c3611debefb
Nandini Singh <nandanithakur531@gmail.com>
2026-07-01 14:10:41 +0530

    Time: 305 ms (49.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 4195-find-users-with-high-token-usage/find-users-with-high-token-usage.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT user_id , COUNT(prompt) AS prompt_count,ROUND( AVG(tokens),2) AS avg_tokens
+FROM prompts 
+GROUP BY user_id
+HAVING COUNT(prompt)>=3 AND MAX(tokens)>AVG(tokens)
+ORDER BY avg_tokens DESC , user_id ASC;
+
