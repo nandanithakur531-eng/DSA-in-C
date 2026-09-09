1b2c92c8c603393b3593fa5d13b35698189be20d
Nandini Singh <nandanithakur531@gmail.com>
2026-09-09 08:58:32 +0530

    Time: 13 ms (2.11%) | Memory: 44.3 MB (8.37%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+       
+        for(int i=0 ; i<n-1 ; i++){
+           int swaps=0;
+           for(int j=0 ; j<n-1-i ; j++){
+             if(nums[j]>nums[j+1]){
+                int temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
+                swaps++;
+             }
+           }
+           if(swaps==0){
+            break;
+           }
+        }
+        for(int ele : nums){
+            System.out.print(ele+" ");
+        }
+    }
+}
